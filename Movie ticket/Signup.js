f3694202587cf37d07189e338db62c283dfee0ff
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2025-02-28 17:32:18 +0530

    Form validation for signup page completed

--- Movie ticket/Signup.js
+++ Movie ticket/Signup.js
@@ -13,11 +13,64 @@ let okk=()=>{
     let errfirst=document.querySelector("#errfirst")
     let errlast=document.querySelector("#errlast")
 
-    if(name===" "){
+    if(name===""){
         errname.innerHTML="Please enter your name";
         errname.style.color="red";
-        document.querySelector("#username").placeholder="Enter name please";
+        //document.querySelector("#username").placeholder="Enter name please";
         return false;
     }
-
+    else if(!(email.includes('@'))&&(email.includes('.com')))
+{
+     erremail.innerHTML="Please enter a valid email";
+     erremail.style.color="red";
+     return false;
+}   
+else if(email===""){
+    erremail.innerHTML="Please enter email";
+    erremail.style.color="red";
+    return false;
 }
+else if(first==="")
+    {
+        errfirst.innerHTML="Enter your first name";
+        errfirst.style.color="red";
+        return false;
+    }
+    else if(last==="")
+    {
+        errlast.innerHTML="Enter your last name";
+        errlast.style.color="red";
+        return false;
+    }
+else if(password==="")
+{
+    errpass.innerHTML="Enter password";
+    errpass.style.color="red";
+    return false;
+}
+else if(confirm===password){
+    return true;
+}
+else if(
+    !(
+        password.match(/[1234567890]/)&&
+        password.match(/[!@#$%^&*()]/)&&
+        password.match(/[a-z]/)&&
+        password.match(/[A-Z]/)
+    )
+){
+    errpass.innerHTML="Enter a strong password";
+    errpass.style.color="red";
+    return false;
+}
+else{
+    errcpass.innerHTML="Password does not match";
+    document.querySelector("#confirm-password").value="";
+    document.querySelector("#confirm-password").focus();
+    errcpass.style.color="red";
+    return false;
+}
+
+} 
+    
+
