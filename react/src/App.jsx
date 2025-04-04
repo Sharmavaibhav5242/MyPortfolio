429c24d806f0d371719e42bf2cd20cea218ea15f
Sharmavaibhav5242 <vaibhavsharma5242@gmail.com>
2025-04-04 23:15:18 +0530

    React bootstrap base single page website

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,19 @@
-import myimage1 from "./image/g1.jpeg"
-import myimage2 from "./image/g2.jpeg"
-import myimage3 from "./image/g3.jpeg"
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
-
 import Carousel from 'react-bootstrap/Carousel';
+import AOS from 'aos'
+import 'aos/dist/aos.css';
+
+
+import img1 from'./image/g4.jpg';
+import img2 from'./image/g5.jpg';
+import img3 from'./image/g6.jpg';
+import img4 from'./image/c1.jpg';
+import img5 from'./image/c2.jpg';
+import img6 from'./image/c3.jpg';
+import img7 from'./image/c4.jpg';
+import img8 from'./image/i1.jpg';
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -15,38 +21,51 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import Image from 'react-bootstrap/Image';
+
+
+
+
+
 const App=()=>{
-    return(
-        <>
-         <Navbar bg="dark" data-bs-theme="dark">
+ 
+
+
+
+
+  return(
+    <>
+    
+    <Navbar bg="dark" data-bs-theme="dark"  >
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home" className='logo'  >GUNS&AMMO</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home" className='nav'>Home</Nav.Link>
+            <Nav.Link href="#features" className='nav'>Features</Nav.Link>
+            <Nav.Link href="#pricing" className='nav'>Pricing</Nav.Link>
+            <Nav.Link href="#pricing" className='nav'>Offers</Nav.Link>
+            <Nav.Link href="#pricing" className='nav'>Newly Arrived</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-
-
-      <Carousel>
+     
+      <Carousel fade>
       <Carousel.Item>
-       <img src={myimage1} width="100%" height="300" />
+        <img src={img1} width="100%" height="500px"/>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src={myimage2} width="100%" height="300" />
+        <img src={img2} width="100%" height="500px"/>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src={myimage3} width="100%" height="300" />
+        <img src={img3} width="100%" height="500px" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
@@ -55,50 +74,69 @@ const App=()=>{
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-
-
-
-    <Container>
-      <Row>
-      <Col><Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={myimage1} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card></Col>
-    <Col><Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={myimage2} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card></Col>
-    <Col><Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={myimage3} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card></Col>
-      </Row>
-    </Container>
+    <br />
 
     
-
-        </>
-    )
+<div className='card'>
+    <Container>
+      <Row>
+      <Col>
+        <Card style={{ width: '18rem',backgroundColor:"black",color:"white",height:'25rem' }}>
+      <Card.Img variant="top" src={img4} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </Col>
+    <Col>
+        <Card style={{ width: '18rem',backgroundColor:"black",color:"white",height:'25rem' }}>
+      <Card.Img variant="top" src={img5} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </Col>
+    <Col>
+        <Card style={{ width: '18rem',backgroundColor:"black",color:"white",height:'25rem' }}>
+      <Card.Img variant="top" src={img6} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </Col>
+    <Col>
+        <Card style={{ width: '18rem',backgroundColor:"black",color:"white",height:'25rem' }}>
+      <Card.Img variant="top" src={img7} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </Col>
+      </Row>
+    </Container>
+    </div>
+     <Image src={img8} width='100%' height='700px' />;
+    </>
+  )
 }
-export default App; 
+export default App;
